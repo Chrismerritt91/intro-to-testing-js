9384faa1e5973e1d4ad90a8a7873bb9c6fad5162
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-04-12 09:23:12 -0500

    complete excersise 4

--- code.js
+++ code.js
@@ -2,6 +2,7 @@
 const helloWorld = function() {
     return "Hello, World!";
 }
-function sayHello() {
-    return ''
-}
+function sayHello(input) {
+    if(input === "Alex"){return "Hello, Alex!"}
+     else{
+    return 'Hello, Jane'}}
